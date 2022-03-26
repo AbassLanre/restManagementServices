92b49c1a1d7347b3eb9f377a38a8183292ebaaec
Abass Lanre <kalanruse@gmail.com>
2022-03-26 18:51:07 +0100

    Initial Commit

---
+++ restManagementService/src/main/java/com/myspring/restmanagementservice/RestManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myspring.restmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestManagementServiceApplication.class, args);
+	}
+
+}
---
+++ restManagementService/src/test/java/com/myspring/restmanagementservice/RestManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring.restmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
